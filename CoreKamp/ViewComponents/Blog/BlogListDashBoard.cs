f3be62f85b433cabc1d13efb26f181322127dce6
AO <ahmet_ozer@hotmail.com>
2021-10-30 17:18:00 +0300

    Ders 65 - 30.10.2021

---
+++ CoreKamp/ViewComponents/Blog/BlogListDashBoard.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreKamp.ViewComponents.Blog
+{
+    public class BlogListDashBoard : ViewComponent
+    {
+        BlogManager blogManager = new BlogManager(new EfBlogRepository());
+        public IViewComponentResult Invoke()
+        {
+            var blogValue = blogManager.GetListWithCategory();
+            return View(blogValue);
+        }
+    }
+}
---
+++ CoreKamp/ViewComponents/Category/CategoryListDashBoard.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreKamp.ViewComponents.Category
+{
+    public class CategoryListDashBoard : ViewComponent
+    {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
+        public IViewComponentResult Invoke()
+        {
+            var categoryValue = categoryManager.GenericGetList();
+            return View(categoryValue);
+        }
+    }
+}

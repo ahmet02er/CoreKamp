f514eb473b9f432c53641ebc7b376db2a2eba5fd
AO <ahmet_ozer@hotmail.com>
2021-12-26 22:13:23 +0300

    Ders 105 - 26.12.2021

---
+++ CoreKamp/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,40 @@
+﻿using CoreKamp.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreKamp.Areas.Admin.Controllers
+{
+    public class ChartController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CategoryChart()
+        {
+            List<CategoryClass> categoryList = new List<CategoryClass>();
+            categoryList.Add(new CategoryClass
+            {
+                categoryName="Yazılım",
+                categoryCount=14   
+            });
+            categoryList.Add(new CategoryClass
+            {
+                categoryName = "Teknoloji",
+                categoryCount = 10
+            });
+            categoryList.Add(new CategoryClass
+            {
+                categoryName = "Spor",
+                categoryCount = 5
+            });
+
+            return Json(new { jsonList = categoryList });
+        }
+    }
+}
--- CoreKamp/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ CoreKamp/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace CoreKamp.Areas.Admin.ViewComponents.Statistic
 {
@@ -18,6 +19,10 @@ namespace CoreKamp.Areas.Admin.ViewComponents.Statistic
             ViewBag.BlogSayisi = blogManager.GenericGetList().Count();
             ViewBag.MesajSayisi = msSqlContext.Contacts.Count();
             ViewBag.YorumSayisi = msSqlContext.Comments.Count();
+            string api = "659c1dd7e1a3af35a72028df2d6dd1b1";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=Sakarya&mode=xml&lang=tr&units=metric&appid="+api;
+            XDocument document = XDocument.Load(connection);
+            ViewBag.sicaklik = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
             return View();
         }
     }

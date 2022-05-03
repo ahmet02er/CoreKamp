6ca249fcebf12d7d189babec814a260739f07f8d
AO <ahmet_ozer@hotmail.com>
2022-05-03 16:39:17 +0300

    Ders 126 - 03-05-2022

--- CoreKamp/ViewComponents/Writer/WriterAboutOnDashboard.cs
+++ CoreKamp/ViewComponents/Writer/WriterAboutOnDashboard.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.Concrete.Context;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -13,10 +15,13 @@ namespace CoreKamp.ViewComponents.Writer
     {
         WriterManager writerManager = new WriterManager(new EfWriterRepository());
         MsSqlContext msSqlContext = new MsSqlContext();
+
         public IViewComponentResult Invoke()
         {
-            var usermail = User.Identity.Name;
-            var writerId = msSqlContext.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterId).FirstOrDefault();
+            var userName = User.Identity.Name;
+            ViewBag.username = userName;
+            var userMail = msSqlContext.Users.Where(x => x.UserName == userName).Select(y => y.Email).FirstOrDefault();
+            var writerId = msSqlContext.Writers.Where(x => x.WriterMail == userMail).Select(y => y.WriterId).FirstOrDefault();
             var writerValue = writerManager.GetWriterById(writerId);
             return View(writerValue);
         }
